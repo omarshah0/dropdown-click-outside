f70433edf1e31234ef980fa64ab5932e87535642
Omar Shah <oemyoem55@gmail.com>
2021-02-03 12:43:16 +0500

    Initial Commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,101 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useState, useEffect, useRef } from "react"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
+const Header = ({ siteTitle }) => {
+  const [showMenu, setShowMenu] = useState(false)
+  const hamburgerRef = useRef()
+  useEffect(() => {
+    function handleDropDown(event) {
+      if (
+        hamburgerRef.current &&
+        !hamburgerRef.current.contains(event.target)
+      ) {
+        setShowMenu(false)
+      }
+    }
+    document.addEventListener("mousedown", handleDropDown)
+    return () => {
+      document.removeEventListener("mousedown", handleDropDown)
+    }
+  }, [])
+  return (
+    <header
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        background: "rebeccapurple",
+        marginBottom: "1.45rem",
+        padding: "1.45rem 3rem",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
+      <div
+        style={{
+          maxWidth: 960,
+        }}
+      >
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+      </div>
+      <div
+        className="dropdown"
+        style={{ position: "relative" }}
+        ref={hamburgerRef}
+      >
+        <div
+          className="hamburger"
           style={{
-            color: `white`,
-            textDecoration: `none`,
+            display: "flex",
+            flexDirection: "column",
+            gap: "4px",
+            cursor: "pointer",
+          }}
+          onClick={() => setShowMenu(true)}
+        >
+          <span
+            style={{ background: "white", width: "20px", height: "4px" }}
+          ></span>
+          <span
+            style={{ background: "white", width: "20px", height: "4px" }}
+          ></span>
+          <span
+            style={{ background: "white", width: "20px", height: "4px" }}
+          ></span>
+        </div>
+        <div
+          className="menu"
+          style={{
+            background: "#D1D5DB",
+            display: "flex",
+            flexDirection: "column",
+            gap: "4px",
+            position: "absolute",
+            top: "30px",
+            right: 0,
+            padding: "1rem 3rem",
+            borderRadius: "10px",
+            visibility: `${showMenu ? "visible" : "hidden"}`,
+            opacity: `${showMenu ? "1" : "0"}`,
           }}
         >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Services</a>
+        </div>
+      </div>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
